61197eec3cd88e9bf66da381afa19e2b0957be80
Berend Kalberg <berend.kalberg@gmail.com>
2020-08-11 16:16:08 +0200

    Added padding for mobile on about section and increased image width for mobile blogposts

--- src/components/About.js
+++ src/components/About.js
@@ -19,14 +19,14 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <div className="masthead-white">
+            <div className="masthead-white sm:px-2">
                 <div>
                     <h2>Student</h2>
                     <br></br>
                     <h3>Creative Media &amp; Game Technologies <span className="text-blue-500"><a href="https://www.hogeschoolrotterdam.nl/opleidingen/bachelor/creative-media-and-game-technologies/voltijd/">@Hogeschool Rotterdam</a></span></h3>
                 </div>
             </div>
-            <div className="masthead-white">
+            <div className="masthead-white sm:px-2">
                 <div>
                     <h2>Gepassioneerd</h2>
                     <br></br>
@@ -37,14 +37,14 @@ const About = () => {
                     </ul>
                 </div>
             </div>
-            <div className="masthead-white">
+            <div className="masthead-white sm:px-2">
                 <div>
                     <h2>Meer weten over mijn werk?</h2>
                     <br></br>
                     <h3>Bekijk mijn <span className="text-blue-500"><a href="/blog">blog</a></span></h3>
                 </div>
             </div>
-            <div className="masthead-white">
+            <div className="masthead-white sm:px-2">
                 <div>
                     <h2>Niewsgierig naar mijn ervaring?</h2>
                     <br></br>
