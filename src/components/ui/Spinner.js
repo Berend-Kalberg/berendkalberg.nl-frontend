ff49203fa605371baefaf9aed9ecd54e7d28891b
Berend Kalberg <berend.kalberg@gmail.com>
2020-09-16 20:35:07 +0200

    Spinner now in middle of 100vh

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -3,14 +3,16 @@ import spinner from '../../assets/spinner.svg'
 
 const Spinner = () => {
     return (
-        <img className="py-20" src={spinner} 
-            style={{ 
-                width: '64px', 
-                margin: 'auto', 
-                display:'block'
-            }} 
-            alt='Loading'
-        />
+        <div className="masthead-white">
+             <img className="py-20" src={spinner} 
+                style={{ 
+                    width: '64px', 
+                    margin: 'auto', 
+                    display:'block'
+                }} 
+            alt='Loading'/>
+        </div>
+       
     )
 }
 
